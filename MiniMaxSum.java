23e287d3a423bc9f9b5a25b4ed910b6bd8d612aa
MarceloBRAmaral <marceloamaral.amaral@gmail.com>
2019-04-21 22:51:12 -0300

    first commit

---
+++ MiniMaxSum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MiniMaxSum {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i=0;i<n;i++){//overall loop
+            for(int j=0;j<n-i-1;j++){//space loop
+                System.out.print(" ");
+            }
+            for(int k=0;k<i+1;k++){//# loop
+                System.out.print("#");
+            }
+            System.out.println("");//jump to the next line
+        }
+    }
+}
